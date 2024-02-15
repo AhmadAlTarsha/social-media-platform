c4fe9bde8f9e737cd6a0a0eb88953e5336ca6c4e
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2024-02-15 21:58:21 +0300

    start get author for  each post function

--- social-media-platform/src/app/components/Posts.tsx
+++ social-media-platform/src/app/components/Posts.tsx
@@ -7,6 +7,7 @@ import React, { useEffect, useState }   from 'react'
 const Posts = () => {
 const [posts,setPosts]=useState([])
 const [comments,setComments]=useState([])
+const [authors,setAuthor]=useState([])
 
 
 
@@ -18,6 +19,25 @@ const getPosts=async()=>{
        const res= await axios.get("https://jsonplaceholder.typicode.com/posts")
        
        setPosts(res?.data)
+    
+       
+    } catch (error) {
+        console.log(error);
+        
+    }
+
+
+
+}
+const getAuthors=async()=>{
+
+    try {
+
+
+       const res= await axios.get("https://jsonplaceholder.typicode.com/users")
+       
+       setAuthor(res?.data)
+    // console.log(res?.data);
     
        
     } catch (error) {
@@ -49,8 +69,11 @@ setComments(res?.data)
 
 }
 
+
+
+
+
 const getComment = (postId) => {
-   console.log(postId);
    
     
     // Filter comments for the current post
@@ -60,10 +83,27 @@ const getComment = (postId) => {
     // Return JSX for each comment
     return filteredComments.map((comment) => (
         <div key={comment.id} className="mt-2">
-            <p className="text-sm font-semibold">{comment.email} says:</p>
+            <p className="text-sm font-semibold">{comment.name} says:</p>
             <p className="text-sm text-gray-600">{comment.body}</p>
         </div>
     ));
+};
+const getPostAuthor = (userId) => {
+   
+    console.log(userId);
+    
+    // Filter comments for the current post
+    // const filteredAuthor = authors.filter(author => posts.userId === author.userId);
+
+
+
+    // Return JSX for each comment
+    // return filteredComments.map((comment) => (
+    //     <div key={comment.id} className="mt-2">
+    //         <p className="text-sm font-semibold">{comment.name} says:</p>
+    //         <p className="text-sm text-gray-600">{comment.body}</p>
+    //     </div>
+    // ));
 };
 
 
@@ -72,6 +112,7 @@ useEffect(()=>{
     
 getPosts()
 getPostComments()
+getAuthors()
 },[])
 
 
@@ -83,7 +124,7 @@ getPostComments()
        <div key={element.id} className="max-w-xl mx-auto my-8 bg-white shadow-lg rounded-lg overflow-hidden">
     <div className="px-6 py-4">
       <div className="font-bold text-xl mb-2"><p> post N.{element.id}</p>
-        {  element.title}</div>
+        {element.title}</div>
       <p className="text-gray-700 text-base">
         {element.body}
       </p>
@@ -92,7 +133,11 @@ getPostComments()
       <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
         By Post Creator
       </span>
+      {
+      getPostAuthor(element?.userId)}
+
     </div>
+    
     <div className="px-6 py-4">
       <h5 className="font-bold text-lg mb-2">Comments</h5>
       {
