bd3e939e506690cada1bc7717f4c64531597f551
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2024-02-16 15:24:07 +0300

    end design welome page

--- social-media-platform/src/app/page.tsx
+++ social-media-platform/src/app/page.tsx
@@ -1,10 +1,13 @@
 import BTN from "@/app/components/Button";
 import Posts from "./components/Posts";
+import HomeScreen from "./components/Home/HomePage";
+
 
 export default function Home() {
   return (
     <main>
-      <Posts></Posts>
+      {/* <Posts></Posts> */}
+      <HomeScreen/>
     </main>
   );
 }
