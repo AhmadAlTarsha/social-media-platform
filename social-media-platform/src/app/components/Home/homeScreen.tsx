e611317a479ad62a82cb48cd9ab61851a0f68411
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2024-02-16 16:52:33 +0300

    finish design loader and start the logic for add it until data ffetshing from API

---
+++ social-media-platform/src/app/components/Home/homeScreen.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import React from 'react'
+
+
+const HomeScreen=()=>{
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <p className="mb-4 text-lg text-gray-800">ConnectSphere is your go-to platform for meaningful social connections. Discover, share, and connect in a vibrant community tailored to your interests.</p>
+        <Link 
+        
+          className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out" href={'/posts'}        >
+          Enter ConnectSphere
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default HomeScreen;
--- social-media-platform/src/app/page.tsx
+++ social-media-platform/src/app/page.tsx
@@ -1,20 +1,13 @@
 
-import Link from "next/link";
-// import Posts from "./posts/page";
-// import HomeScreen from "./Home/page";
+
+import HomeScreen from "./components/Home/homeScreen";
 
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-    <div className="text-center">
-      <p className="mb-4 text-lg text-gray-800">ConnectSphere is your go-to platform for meaningful social connections. Discover, share, and connect in a vibrant community tailored to your interests.</p>
-      <Link 
-    
-        className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out" href='/posts'        >
-        Enter ConnectSphere
-      </Link>
-    </div>
-  </div>
+    <main>
+   
+      <HomeScreen/>
+    </main>
   );
 }
