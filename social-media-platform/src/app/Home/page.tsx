53d40883b9504d283afea234d2f6732b1736bd18
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2024-02-16 16:35:42 +0300

    complete routing page ussing Link

--- social-media-platform/src/app/Home/page.tsx
+++ social-media-platform/src/app/Home/page.tsx
@@ -1,20 +1,11 @@
+import Link from 'next/link';
 import React from 'react'
 
 
-const HomeScreen=()=>{
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="text-center">
-        <p className="mb-4 text-lg text-gray-800">ConnectSphere is your go-to platform for meaningful social connections. Discover, share, and connect in a vibrant community tailored to your interests.</p>
-        <button 
-      
-          className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out"
-        >
-          Enter ConnectSphere
-        </button>
-      </div>
-    </div>
-  );
-}
+// const HomeScreen=()=>{
+//   return (
+  
+//   );
+// }
 
-export default HomeScreen;
+// export default HomeScreen;
--- social-media-platform/src/app/page.tsx
+++ social-media-platform/src/app/page.tsx
@@ -1,13 +1,20 @@
 
-import Posts from "./posts/page";
-import HomeScreen from "./Home/page";
+import Link from "next/link";
+// import Posts from "./posts/page";
+// import HomeScreen from "./Home/page";
 
 
 export default function Home() {
   return (
-    <main>
-      <Posts></Posts>
-      {/* <HomeScreen/> */}
-    </main>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="text-center">
+      <p className="mb-4 text-lg text-gray-800">ConnectSphere is your go-to platform for meaningful social connections. Discover, share, and connect in a vibrant community tailored to your interests.</p>
+      <Link 
+    
+        className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out" href={'/posts'}        >
+        Enter ConnectSphere
+      </Link>
+    </div>
+  </div>
   );
 }
